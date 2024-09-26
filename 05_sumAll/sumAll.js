f836e555815d964acc1862a885daf71018ec5270
Thibaud Joel Hadamczik <thadamczik@gmail.com>
2024-09-26 13:56:26 +0200

    Solve fifth exercise

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,4 +1,20 @@
-const sumAll = function() {
+const sumAll = function (firstInteger, secondInteger) {
+    if (typeof firstInteger !== "number" || typeof secondInteger !== "number") {
+        return "ERROR";
+    }
+    if (firstInteger % 1.0 !== 0 || secondInteger % 1.0 !== 0) {
+        return "ERROR";
+    }
+    if (firstInteger < 0 || secondInteger < 0) {
+        return "ERROR";
+    }
+    const smallerInteger = Math.min(firstInteger, secondInteger);
+    const largerInteger = Math.max(firstInteger, secondInteger);
+    let outputSum = 0;
+    for (let summand = smallerInteger; summand <= largerInteger; summand++) {
+        outputSum += summand;
+    }
+    return outputSum;
 
 };
 
