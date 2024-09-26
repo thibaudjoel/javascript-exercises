2890150899746e5a3e7abd100ab6d89463daa9ec
Thibaud Joel Hadamczik <thadamczik@gmail.com>
2024-09-26 13:44:49 +0200

    Solve fourth exercise

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,4 +1,15 @@
-const removeFromArray = function() {
+const removeFromArray = function (array, ...elementsToRemove) {
+    const outputArray = [];
+    const removeArray = elementsToRemove;
+    outer: for (const element of array) {
+        for (const elementToRemove of removeArray) {
+            if (element === elementToRemove) {
+                continue outer;
+            }
+        }
+        outputArray.push(element);
+    }
+    return outputArray;
 };
 
 // Do not edit below this line
